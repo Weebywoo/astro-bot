511f2b10ac0e500a3ebf33bedabc07d49c6d442d
Weebywoo <kosmokunert@gmail.com>
2024-07-14 13:21:32 +0200

    Removed health endpoint

--- main.py
+++ main.py
@@ -2,9 +2,7 @@ import asyncio
 import os
 
 import discord
-from fastapi import FastAPI
 
-from src.api import router as api_router
 from src.bot import Bot
 from src.commands import NasaApiCommands
 
@@ -19,8 +17,5 @@ async def main():
     async with Bot(command_prefix=">", intents=intents, custom_commands=commands) as bot:
         await bot.start(os.environ.get('DISCORD_TOKEN'))
 
-
-app = FastAPI()
-app.include_router(api_router)
 
 asyncio.run(main())
