e44e11926c02383bd635e66816b2e0a90e9442cc
Weebywoo <kosmokunert@gmail.com>
2024-07-22 18:19:42 +0200

    Renamed env vars

--- src/config.py
+++ src/config.py
@@ -4,5 +4,5 @@ nasa_api_urls: dict[str, str] = {
     'APOD': 'https://api.nasa.gov/planetary/apod'
 }
 
-discord_token: str = os.environ.get('discord_token')
-nasa_token: str = os.environ.get('nasa_token')
+discord_token: str = os.environ.get('DISCORD_API_KEY')
+nasa_token: str = os.environ.get('NASA_API_KEY')
